c05a036cee71cc17a4f3584cb519e641dc49d6aa
yeoV <ys_lee1017@naver.com>
2024-12-19 10:02:47 +0900

    🌲day9 part1 - two pointer

--- day9/part1.py
+++ day9/part1.py
@@ -1,33 +1,36 @@
 import sys
 from collections import deque
+
 read = sys.stdin.readline
 
 disk_map = list(map(int, read()))
+
 
 def create_blocks(disk_map) -> list:
     flag = True
     idx = 0
     blocks = list()
     for disk in disk_map:
-        if flag:    # file turn
+        if flag:  # file turn
             blocks.extend([idx] * disk)
             idx += 1
         else:
             blocks.extend(["."] * disk)
-        flag = not flag # reverse
+        flag = not flag  # reverse
 
     return blocks
 
 
 def compact_disk(blocks):
-    for s_idx, val in enumerate(blocks):
-        if val == ".":
-            for e_idx in range(len(blocks)-1, 0, -1):
-                if blocks[e_idx] != "." and s_idx < e_idx:
-                    blocks[s_idx], blocks[e_idx] = blocks[e_idx], blocks[s_idx]
-                    break
+    # Two pointer
+    e_idx = len(blocks) - 1
 
-    return  blocks
+    for s_idx in range(len(blocks)):
+        if blocks[s_idx] == ".":
+            while blocks[e_idx] == "." and s_idx < e_idx:
+                e_idx -= 1
+            blocks[s_idx], blocks[e_idx] = blocks[e_idx], blocks[s_idx]
+    return blocks
 
 
 def calc_checksum(disk):
